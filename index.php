738cc642594ca8a2469ca5df356a47fa0fc831fc
Manh-Hoang-07 <vumanhhoang140799@gmail.com>
2023-07-10 23:48:03 +0700

    hoangvm

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Web PHP</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+
+        </div>
+        <div class="menu">
+            <ul class="list-menu">
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="#">Danh mục sản phẩm</a></li>
+                <li><a href="#">Giỏ hàng</a></li>
+                <li><a href="#">Tin tức</a></li>
+                <li><a href="#">Liên hệ</a></li>
+            </ul>
+        </div>
+        <div class="main" id="main">
+            <div class="sidebar">
+                Sidebar
+            </div>
+            <div class="main-content"></div>
+        </div>
+        <div class="clear"></div>
+        <div class="footer">
+            <p>footer</p>
+        </div>
+    </div>
+</body>
+</html>
